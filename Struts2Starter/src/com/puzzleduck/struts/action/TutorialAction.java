e5c38aeebe7a1886299d6433f117e810fa26158a
PuZZleDucK <puzzleduck@gmail.com>
2013-03-22 16:55:58 +1100

    initial struts project commit

---
+++ Struts2Starter/src/com/puzzleduck/struts/action/TutorialAction.java
@@ -0,0 +1,15 @@
+package com.puzzleduck.struts.action;
+
+public class TutorialAction {
+	
+	
+	public String execute() {
+		
+		
+		System.out.printf("Exec!");
+		
+		
+		return "success";
+	}
+
+}
